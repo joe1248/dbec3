f2b02927217554cf2dcd73332cbe493418df016b
Joseph Barban <joseph@thenextad.com>
2018-01-12 12:52:53 +0100

    dbec3:  login works from db

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,15 +6,16 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 //use Symfony\Component\Routing\Annotation\Route;
- 
+use Symfony\Component\Security\Core\User\UserInterface;
+
 class IndexController extends Controller
 {
-    public function index()
+    public function index(UserInterface $user = null)
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $number = mt_rand(0, 100);
-
         return new Response(
-            '<html><body>Index Lucky number: '.$number.'</body></html>'
+            '<html><body>Index Lucky number: '.$number. 'Well hi there '.$user->getUsername() . '</body></html>'
         );
     }
 }
