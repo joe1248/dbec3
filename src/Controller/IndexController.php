299446ef92685b0b7dbb1975afdb8ef9b4f28cc2
Joseph Barban <joseph@thenextad.com>
2018-01-12 13:47:01 +0100

    dbec3:  remove annotations

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends Controller
 {
--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,19 +5,15 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class LuckyController  extends Controller 
 {
-    /**
-     * route("/lucky", name="LuckyRoute")
-     */
     public function number(UserInterface $user = null)
     {
         $number = mt_rand(0, 100);
 
-        //$this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $number = mt_rand(0, 100);
         return new Response(
             '<html><body>BINGO Lucky number: '.$number. 'Well hi there '.$user->getUsername() . ' <a href="/logout">Logout</a></body></html>'
