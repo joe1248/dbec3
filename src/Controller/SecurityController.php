0da8c1f4d89e02663b8592ec6446ed8697af222e
Joseph Barban <joseph@thenextad.com>
2018-01-12 10:58:26 +0100

    dbec3:  Security controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+// src/Controller/SecurityController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    private $authUtils;
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request)
+    {
+        $this->authUtils =  $this->get(AuthenticationUtils::class);//new ();
+        // get the login error if there is one
+        $error = $this->authUtils->getLastAuthenticationError();
+    
+        // last username entered by the user
+        $lastUsername = $this->authUtils->getLastUsername();
+    
+        return $this->render('security/login.html.php', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
