bc70425b558d5aa023ded1260f67903d1e260f3b
joseph <joseph@thenextad.com>
2018-01-24 18:38:27 +0100

    DBEC3: replace karma + jasmine by Jest + vue-test-utils

--- spec/components/AlertSpec.js
+++ spec/components/AlertSpec.js
@@ -1,6 +1,6 @@
 describe("Alert component", function() {
 
-    var c = require('./../../../resources/assets/js/components/Alert.vue');
+    var c = require('./../../assets/components/Alert.vue');
 
     it('should have data', function () {
         expect(typeof c.data).toBe('function');
--- spec/index.js
+++ spec/index.js
@@ -1,5 +1,5 @@
 // require all test files using special webpack feature
 // https://webpack.github.io/docs/context.html#require-context
 
-var testsContext = require.context('.', true, /\.spec$/)
+var testsContext = require.context('.', true, /\AlertSpec$/)
 testsContext.keys().forEach(testsContext)
